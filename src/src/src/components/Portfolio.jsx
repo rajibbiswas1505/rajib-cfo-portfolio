cc538244388c38eeb9326f5a21c6dcbcdca062e1
rajibbiswas1505 <rajib.biswas1505@gmail.com>
2025-09-20 20:42:44 +0530

    Update Portfolio.jsx

--- src/src/src/components/Portfolio.jsx
+++ src/src/src/components/Portfolio.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import Hero from './Hero';
 
 const Portfolio = () => {
-  const [activeSection, setActiveSection] = useState('hero');
-
   return (
     <div className="portfolio-container">
-      <Header activeSection={activeSection} />
-      <section id="hero">
-        <Hero setActiveSection={setActiveSection} />
-      </section>
+      <Header />
+      <Hero />
     </div>
   );
 };
